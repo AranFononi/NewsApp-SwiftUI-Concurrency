20d111a23c799cc121b26a6f3c82330e9159736b
Yasin Erdemli <yasinerdemli35@icloud.com>
2025-03-05 13:30:29 +0300

    some little cahnges in NewsListScreen

--- NewsApp/Views/NewsListScreen.swift
+++ NewsApp/Views/NewsListScreen.swift
@@ -3,35 +3,29 @@
 import SwiftUI
 
 struct NewsListScreen: View {
-    
-    let newsSource: NewsSourceViewModel
-    @StateObject private var newsArticleListViewModel = NewsArticleListViewModel()
-    
+    @StateObject private var viewModel = NewsArticleListViewModel()
+    let newsSource: NewsSource
     var body: some View {
-        
-        
-        List(newsArticleListViewModel.newsArticles, id: \.id) { newsArticle in
+        List(viewModel.newsArticles) { newsArticle in
                 NewsArticleCell(newsArticle: newsArticle)
         }
         .listStyle(.plain)
-        .task({
-            await newsArticleListViewModel.getNewsBy(sourceId: newsSource.id)
-        })
+        .task {
+            await viewModel.getNewsBy(sourceId: newsSource.id)
+        }
         .navigationTitle(newsSource.name)
-        
     }
 }
 
 struct NewsListScreen_Previews: PreviewProvider {
     static var previews: some View {
-        NewsListScreen(newsSource: NewsSourceViewModel.default)
+        NewsListScreen(newsSource: .example)
     }
 }
 
 struct NewsArticleCell: View {
-    
-    let newsArticle: NewsArticleViewModel
-    
+    let newsArticle: NewsArticle
+
     var body: some View {
         HStack(alignment: .top) {
             AsyncImage(url: newsArticle.urlToImage) { image in
@@ -41,7 +35,7 @@ struct NewsArticleCell: View {
                 ProgressView("Loading...")
                     .frame(maxWidth: 100, maxHeight: 100)
             }
-            
+
             VStack {
                 Text(newsArticle.title)
                     .fontWeight(.bold)
