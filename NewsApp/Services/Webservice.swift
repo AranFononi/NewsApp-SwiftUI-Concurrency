e6c243aae39398d49070a3ba59ffc50102fe8528
Yasin Erdemli <yasinerdemli35@icloud.com>
2025-03-05 13:15:22 +0300

    use modern concurrency in the WebService.

--- NewsApp/Services/Webservice.swift
+++ NewsApp/Services/Webservice.swift
@@ -1,59 +1,20 @@
 
 import Foundation
 
-enum NetworkError: Error {
-    case badUrl
-    case invalidData
-    case decodingError
-}
-
-class Webservice {
-    
-    
-    func fetchSourcesAsync(url: URL?) async throws -> [NewsSource] {
-        guard let url = url else {
-            return []
-        }
-        
+actor Webservice {
+    let decoder = JSONDecoder()
+    func fetchSources() async throws -> NewsSources {
+        let url = try URLHelper.sources()
         let (data, _) = try await URLSession.shared.data(from: url)
-        let newsSourceResponse = try? JSONDecoder().decode(NewsSourceResponse.self, from: data)
-        
-        return newsSourceResponse?.sources ?? []
+        let newsSourceResponse = try decoder.decode(NewsSourceResponse.self, from: data)
+
+        return newsSourceResponse.sources
     }
-    
-    
-    func fetchNewsAsync(by sourceId: String, url: URL?) async throws -> [NewsArticle] {
-        try await withCheckedThrowingContinuation { continuation in
-            fetchNews(by: sourceId, url: url) { result in
-                switch result {
-                case .success(let newsArticles):
-                    continuation.resume(returning: newsArticles)
-                case .failure(let error):
-                    continuation.resume(throwing: error)
-                }
-            }
-        }
+
+    func fetchNews(by sourceID: String) async throws -> NewsArticles {
+        let url = try URLHelper.topHeadlines(source: sourceID)
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let newsArticlesResponse = try decoder.decode(NewsArticleResponse.self, from: data)
+        return newsArticlesResponse.articles
     }
-    
-    private func fetchNews(by sourceId: String, url: URL?, completion: @escaping (Result<[NewsArticle], NetworkError>) -> Void) {
-        
-        guard let url = url else {
-            completion(.failure(.badUrl))
-            return
-        }
-            
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            
-            guard let data = data, error == nil else {
-                completion(.failure(.invalidData))
-                return
-            }
-            
-            let newsArticleResponse = try? JSONDecoder().decode(NewsArticleResponse.self, from: data)
-            completion(.success(newsArticleResponse?.articles ?? []))
-            
-        }.resume()
-        
-    }
-    
 }
