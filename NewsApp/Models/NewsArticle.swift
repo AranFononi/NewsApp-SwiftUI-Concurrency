8970a0dab288ba134f2eee20f963ce6b491c8a5d
Yasin Erdemli <yasinerdemli35@icloud.com>
2025-03-05 13:13:08 +0300

    make NewsArticle identifiable with its author, title, publishedAt combination. Use codingKeys to decode everything to be what is needed inside the app instead of basic types.

--- NewsApp/Models/NewsArticle.swift
+++ NewsApp/Models/NewsArticle.swift
@@ -1,18 +1,51 @@
-
-
 import Foundation
 
+typealias NewsArticles = [NewsArticle]
+
 struct NewsArticleResponse: Decodable {
-    let articles: [NewsArticle]
+    let articles: NewsArticles
 }
 
-struct NewsArticle: Decodable {
-    let author: String?
+struct NewsArticle: Decodable, Identifiable {
+    var id: String {
+        author + title + publishedAt
+    }
+    let author: String
     let title: String
-    let description: String?
-    let url: String?
-    let content: String?
+    let description: String
+    let url: URL?
+    let content: String
     let publishedAt: String
-    let urlToImage: String?
-}
+    let urlToImage: URL?
 
+    enum CodingKeys: String, CodingKey {
+        case author
+        case title
+        case description
+        case url
+        case content
+        case publishedAt
+        case urlToImage
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.author = (try? container.decode(String.self, forKey: .author)) ?? ""
+        self.title = try container.decode(String.self, forKey: .title)
+        self.description = (try? container.decode(String.self, forKey: .description)) ?? ""
+        if let urlString = try? container.decode(String.self, forKey: .url), let url = URL(string: urlString) {
+            self.url = url
+        } else {
+            self.url = nil
+        }
+        self.content = (try? container.decode(String.self, forKey: .content)) ?? ""
+        self.publishedAt = try container.decode(String.self, forKey: .publishedAt)
+
+        if let urlToImageString = try? container.decode(String.self, forKey: .urlToImage), let urlToImage = URL(string: urlToImageString) {
+            self.urlToImage = urlToImage
+        } else {
+            self.urlToImage = nil
+        }
+    }
+}
