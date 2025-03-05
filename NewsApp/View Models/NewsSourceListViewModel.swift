8b3f43ce6cd93c850e88a2864130ad0b139fcd41
Yasin Erdemli <yasinerdemli35@icloud.com>
2025-03-05 13:25:41 +0300

    get rid of NewsSourceViewModel (it is not actually a view model), use new Webservice.

--- NewsApp/View Models/NewsSourceListViewModel.swift
+++ NewsApp/View Models/NewsSourceListViewModel.swift
@@ -2,42 +2,16 @@
 
 import Foundation
 
-@MainActor
 class NewsSourceListViewModel: ObservableObject {
-    
-    @Published var newsSources: [NewsSourceViewModel] = []
-    
+    @Published var newsSources: NewsSources = []
+    let service = Webservice()
+
     func getSources() async {
-        
         do {
-            let newsSources = try await Webservice().fetchSourcesAsync(url: Constants.Urls.sources)
-                self.newsSources = newsSources.map(NewsSourceViewModel.init)
+            let newsSources = try await service.fetchSources()
+            self.newsSources = newsSources
         } catch {
             print(error)
         }
-        
-    }
-    
-}
-
-struct NewsSourceViewModel {
-    
-    fileprivate var newsSource: NewsSource
-    
-    var id: String {
-        newsSource.id
-    }
-    
-    var name: String {
-        newsSource.name
-    }
-    
-    var description: String {
-        newsSource.description
-    }
-    
-    static var `default`: NewsSourceViewModel {
-        let newsSource = NewsSource(id: "abc-news", name: "ABC News", description: "This is ABC news")
-        return NewsSourceViewModel(newsSource: newsSource)
     }
 }
